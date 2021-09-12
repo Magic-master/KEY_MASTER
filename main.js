5c30b6788e0a380c62a715387a8c859b5ec03204
Magic-master <85842164+Magic-master@users.noreply.github.com>
2021-09-12 13:26:43 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function uploadimg() {
 	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
-window.addEventListener("keydown");
+window.addEventListener("keydown",my_keydown);
 
 function my_keydown(e)
 {
@@ -29,18 +29,39 @@ function my_keydown(e)
 	
 		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
 		{
-aplhabetkey();
-document.getElementById("d1").innerHtml="You pressed Aplhabet Key";
+alphabetkey();
+document.getElementById("d1").innerHTML="You pressed Alphabet Key";
 console.log("alphabet key");
 		}
 	
+else if(keyPressed >=48 && keyPressed<=57)
+{
+	numberkey();
+	document.getElementById("d1").innerHTML="You pressed Number Key";
+	console.log("Number key");
+}
+
+else if(keyPressed >=37 && keyPressed<=40)
+{
+	arrowkey();
+	document.getElementById("d1").innerHTML="You pressed Arrow Key";
+	console.log("Arrow Key");
+}
+
+else if(keyPressed ==17 ||keyPressed==18||keyPressed==27)
+{
+specialkey();
+document.getElementById("d1").innerHTML="you pressed ctrl/esc/alt";
+console.log("special key");
+}
+
 	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
 	}
 }
 
-function aplhabetkey()
+function alphabetkey()
 {
    img_image="Alpkey.png";
    add();
@@ -48,14 +69,18 @@ function aplhabetkey()
 }
 function numberkey()
 {
-	
+ img_image="numkey.png"
+ add();
 }
 function arrowkey()
 {
+	img_image="Arrkey.png"
+ add();
 }
 function specialkey()
 {
-	
+	img_image="spkey.png"
+	add();
 }
 function otherkey()
 {
